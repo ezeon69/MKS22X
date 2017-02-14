94f2a498f8f6ee6de7372eba21547b0f196288a8
Jiajun Luo <luo.jiajun.jiajun@gmail.com>
2017-02-13 23:32:50 -0500

    finished QueenBoard

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,9 +1,10 @@
 public class QueenBoard{
-    private int[][]board;
+    public int[][]board;
     private int solutionCount;
     
     public QueenBoard(int size){
 	board = new int[size][size];
+	solutionCount = 0;
     }
 
     /**
@@ -19,35 +20,29 @@ public class QueenBoard{
 	solveH(0);
     }
 
-    private void addQueen(int r, int c){
-	if (board[r][c] == 0){
-	    board[r][c] = 1;
-	    for(int col = c; col < board.length; col++){
-		board[r][col] = -1;
+    private void addQueen(int row, int col){
+        for (int counter = 0; counter < Math.pow(board.length,2); counter++){
+	    if (counter % board.length == col || counter / board.length == row || Math.abs(counter / board.length - row ) == Math.abs( counter % board.length - col)){
+		if (board[counter / board.length][counter % board.length] != -1){
+		    board[counter / board.length][counter % board.length]++;
+		}
 	    }
-	    for(int row = r, col = c; col < board.length && row < board.length; col++, row++){
-		board[row][col] = -1;
+	}
+	board[row][col] = -1;
+    }
+
+    private void removeQueen(int row, int col){
+        board[row][col] = 0;	
+	for (int counter = 0; counter < Math.pow(board.length,2); counter++){
+	    if ((counter % board.length == col || counter / board.length == row || Math.abs(counter / board.length - row) == Math.abs(counter % board.length - col)) && counter != board.length * row + col){
+		if (board[counter / board.length][counter % board.length ] != -1) 
+		    board[counter / board.length][counter % board.length ]--;
+		else
+		    board[row][col]++;
 	    }
-	    for(int row = r, col = c; col < board.length && row > 0; col++, row--){
-		board[row][col] = -1;
-	    }	    
 	}
     }
 
-    private void removeQueen(int r, int c){
-        if (board[r][c] == 1){
-	    board[r][c] = 0;
-	    for(int col = c; col < board.length; col++){
-		board[r][col] = 0;
-	    }
-	    for(int row = r, col = c; col < board.length && row < board.length; col++, row++){
-		board[row][col] = 0 ;
-	    }
-	    for(int row = r, col = c; col < board.length && row > 0; col++, row--){
-		board[row][col] = 0 ;
-	    }	    
-	}
-    }
 
     // nothing = 0, queen = 1, conflict == -1
     private boolean solveH(int col){
@@ -58,31 +53,60 @@ public class QueenBoard{
         for (int row =  0; row < board.length; row++){
 	    if (board[row][col] == 0){
 		addQueen(row,col);
-		if solveH(col + 1){
+		if (solveH(col + 1)){
 			return true;
 		    }
 		else{
-		    removeQueen{row,col};
+		    removeQueen(row,col);
 		}
 	    }
 	}
 	return false;	
     }
     public void countSolutions(){
-
+	solveAll(0);
     }
+
+    public void solveAll(int col){
+	if (col >= board.length){
+	    solutionCount++;
+	}
+	else{
+	    for (int row = 0; row < board.length; row++){
+		if (board[row][col] == 0){
+		    addQueen(row,col);
+		    solveAll(col + 1);
+		    removeQueen(row,col);
+		}
+	    }
+	}
+    }
+    
     /**
      *@return the number of solutions found, or -1 if the board was never solved.
      *The board should be reset after this is run.    
      */
     public int getSolutionCount(){
-    	return -1;
+    	return solutionCount;
     }
+    
     /**toString
      *and all nunbers that represent queens are replaced with 'Q' 
      *all others are displayed as underscores '_'
      */
     public String toString(){
-    	return "";
+        String total = "";
+	for (int[]row : board){
+	    for (int col : row){
+		if (col == -1){
+		    total += "Q";
+		}
+		else{
+		    total += "_";
+		}
+	    }
+	    total += "\n";
+	}
+	return total;
     }
 }
