571d49a63ec1d45b3795874915fce10edae54e20
Jiajun Luo <luo.jiajun.jiajun@gmail.com>
2017-03-12 22:06:38 -0400

    started silver

---
+++ 05USACO/Grass.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Grass{
+    private int[][] ary1;
+    private int[][] ary2;
+    private int steps1;
+    private int steps2;
+
+    private int steps;
+
+}
