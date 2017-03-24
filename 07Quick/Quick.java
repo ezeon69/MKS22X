ad0bdb9741b61c602876193a2072c7b2b3a4a630
Jiajun Luo <luo.jiajun.jiajun@gmail.com>
2017-03-24 19:47:10 -0400

    rename, test quick

---
+++ 07Quick/Quick.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.lang.*;
+
+public class Quick{
+
+    public static int quickselect(int[] data, int k, int start, int end){
+	int part = partition(data, 0, data.length);
+	
+        if (part == k){
+	    return data[part];
+	}
+	if (part > k){
+	    return quickselect(data, k, part - 1, end);
+	}
+	return quickselect(data, k, start, part - 1);
+    }    
+    
+    public static int partition(int[] data, int start, int end){
+	double randomFloat = (Math.random() * data.length);
+	int random = (int) randomFloat;
+	
+	int pivot = data[start];
+	
+        swap(random,pivot,data);
+	
+	int lt = start;
+	int i = start;
+	int j = end;
+	while (i <= j){
+	    if (data[i] < pivot){
+		lt++;
+	    }
+	    else if (data[i] == pivot){
+		i++;
+	    }
+	    else{
+		swap(i,j,data);
+		j--;
+	    }
+	}
+	swap(start,j,data);
+	return pivot;
+    }
+	    
+    private static void swap(int x, int y, int[] data){
+        int temp = data[x];
+        data[x] = data[y];
+        data[y] = temp;
+    }
+    
+    public static void main(String[]args){
+	int[] ary = {8,1,2,7,4,3,6,5,3,3,3,3,3,3,3};
+	partition(ary,0,ary.length);
+	System.out.println(Arrays.toString(ary));
+    }
+    
+}
+	
