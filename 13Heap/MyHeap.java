517177876dd1821ebf8d5c0620ff91c81adf2118
Jiajun Luo <luo.jiajun.jiajun@gmail.com>
2017-05-04 14:42:25 -0400

    worked on heap

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,16 +1,26 @@
+import java.util.*;
+
 public class MyHeap{
     
     private List<String> data;
     private int size;
     private int max;
     
-    
     public MyHeap(){
-	
+	data = new List<String>;
+	size = 0;
+	max = 1;
     }
 
     public MyHeap(boolean t){
-	
+	if (t){
+	    new MyHeap();
+	}
+	else{
+	    data = new List<String>;
+	    size = 0;
+	    max = 0;
+	}
     }
     
     public int size(){
@@ -18,15 +28,22 @@ public class MyHeap{
     }
     
     public void add(String s){
-	
+	data.add(s);
+	size++;
+	pushUp();
     }
 
     public String remove(){
-	
+	String total = peek();
+	data[0] = data[size];
+	data[size] = null;
+	size--;
+	pushDown();
+	return total;
     }
 
     public String peek(){
-	
+	return data[0];
     }
 
     private void pushUp(){
@@ -34,7 +51,7 @@ public class MyHeap{
     }
 
     private void pushDown(){
-
+	
     }
 
 
