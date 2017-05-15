85df92165ad37b4fafedaecf96f7910c7c1c58e1
Jiajun Luo <luo.jiajun.jiajun@gmail.com>
2017-05-15 14:43:29 -0400

    made skeleton

---
+++ 15SuperMazeSolver/Location.java
@@ -0,0 +1,28 @@
+public class Location implements Comparable<Location>{
+    
+    private int row;
+    private int col;
+    private Location previous;
+    private int distanceToStart;
+
+    private int distToGoal(){
+    
+    }
+
+    private boolean aStar(){
+
+    }
+
+    public Location(int r, int c, Location previous,  int distToStart, int distToGoal){
+	
+    }
+    
+    public Location(int r, int c, Location previous , int distToStart, int distToGoal, boolean aStar){
+
+    }
+    
+    public int compareTo(Location Other){
+
+    }
+
+}
---
+++ 15SuperMazeSolver/MazeSolver.java
@@ -0,0 +1,34 @@
+public class MazeSolver{
+
+    private Maze board;
+    
+    public void solve(){
+
+    }
+    
+    public void solve(int){
+	return 0;
+    }
+
+    public MazeSolver(String filename){
+	
+    }
+    
+    public MazeSolver(String filename, boolean animate){
+	
+    }
+    
+    public class Maze{
+	
+	new char[][] grid;
+	new int maxrows;
+	new int maxcols;
+	new Location mazeEnd;
+	new Location mazeStart;
+
+	public String toString(){
+	    return "";
+	}
+	
+	public void Maze(String 
+	
